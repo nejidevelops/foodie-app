13da27429afd95b94f55573a2c0211701a4ead3f
Newton Bofulo <newtonombese1@gmail.com>
2024-03-16 06:19:26 +0300

    Setting up a new background image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className='bg-[url(https://images.pexels.com/photos/616401/pexels-photo-616401.jpeg?auto=compress&cs=tinysrgb&w=1600)] h-[100vh] bg-cover bg-center bg-no-repeat flex flex-col justify-center text-white px-[40px] sm:px-[140px]'>
+    <div className='bg-[url(https://images.pexels.com/photos/1482803/pexels-photo-1482803.jpeg?auto=compress&cs=tinysrgb&w=1600)] h-[100vh] bg-cover bg-center bg-no-repeat flex flex-col justify-center text-white px-[40px] sm:px-[140px]'>
       <div className='w-3/4 flex flex-col gap-y-4'>
         <h1 className='text-8xl'>Enjoy your <br/>delicious Food</h1>
         <p className='text-[24px] w-[580px]'>Just order and wait for a while we&apos;ll be there at your door. Delivering the best foods for you. We are delivering the best foods.</p>
