d4a5b38f021ee49963bcc873877c7baeaafd2aa1
Newton Bofulo <newtonombese1@gmail.com>
2024-03-16 07:33:35 +0300

    Import about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import About from './components/About'
 import Home from './components/Home'
 import { StickyNavbar } from './components/Navbar'
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <StickyNavbar />
       <Home />
+      <About />
     </>
   )
 }
