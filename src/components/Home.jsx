747f459736b38be150d284f3831e36dca3d5fb04
Newton Bofulo <newtonombese1@gmail.com>
2024-03-16 06:29:58 +0300

    Setting up the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ function Home() {
       <div className='w-3/4 flex flex-col gap-y-4'>
         <h1 className='text-8xl'>Enjoy your <br/>delicious Food</h1>
         <p className='text-[24px] w-[580px]'>Just order and wait for a while we&apos;ll be there at your door. Delivering the best foods for you. We are delivering the best foods.</p>
-        <div className='flex gap-4'>
-          <button className='bg-[#E14B00] text-white font-bold px-4 py-2 rounded-[6px] border-0'>Order Now</button>
-          <button className='border border-[#E14B00] rounded-[6px] text-[#E14B00] px-4 py-2'>View Menu</button>
+        <div className='flex gap-4 mt-12'>
+          <button className='bg-[#E14B00] text-white font-bold px-4 py-2 rounded-[6px] border-0 hover:bg-white hover:text-[#E14B00]'>Order Now</button>
+          <button className='border border-[#E14B00] rounded-[6px] text-[#E14B00] px-4 py-2 hover:border-white hover:text-white'>View Menu</button>
         </div>
       </div>
       <div></div>
