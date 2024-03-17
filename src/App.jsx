5494bd1e4b9fa99c521219a44c64c45fc9193b88
Newton Bofulo <newtonombese1@gmail.com>
2024-03-17 14:01:47 +0300

    Add Menu component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import About from './components/About'
 import Home from './components/Home'
+import Menu from './components/Menu'
 import { StickyNavbar } from './components/Navbar'
 import Service from './components/Service'
 
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Service />
+      <Menu />
     </>
   )
 }
