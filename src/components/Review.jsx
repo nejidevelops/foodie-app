c5c8f9e9b2ed320cf9a5be9083f0fe41457e8fe4
Newton Bofulo <newtonombese1@gmail.com>
2024-03-20 08:53:59 +0300

    Setting up the review section

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -10,8 +10,10 @@ function Review() {
 
         <div className="mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8">
           <blockquote className="rounded-lg bg-[#3f3f3f] p-6 shadow-sm sm:p-8">
-          <p className="mb-4 text-white">
-              Our restautant is open 24 hours and we are providing the best services for Our restaurant is open 24 hours and we are providing our honorable clients.
+            <p className="mb-4 text-white">
+              Our restautant is open 24 hours and we are providing the best
+              services for Our restaurant is open 24 hours and we are providing
+              our honorable clients.
             </p>
 
             <div className="flex items-center gap-4">
@@ -22,7 +24,7 @@ function Review() {
               />
 
               <div>
-              <p className="mt-0.5 text-lg font-medium text-white">
+                <p className="mt-0.5 text-lg font-medium text-white">
                   Bofulo Newton
                 </p>
                 <div className="flex justify-center gap-0.5 text-[#ff5001]">
@@ -67,15 +69,15 @@ function Review() {
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                   </svg>
                 </div>
-
-
               </div>
             </div>
           </blockquote>
 
           <blockquote className="rounded-lg bg-[#3f3f3f] p-6 shadow-sm sm:p-8">
-          <p className="mb-4 text-white">
-              Our restautant is open 24 hours and we are providing the best services for Our restaurant is open 24 hours and we are providing our honorable clients.
+            <p className="mb-4 text-white">
+              Our restautant is open 24 hours and we are providing the best
+              services for Our restaurant is open 24 hours and we are providing
+              our honorable clients.
             </p>
 
             <div className="flex items-center gap-4">
@@ -86,7 +88,7 @@ function Review() {
               />
 
               <div>
-              <p className="mt-0.5 text-lg font-medium text-white">
+                <p className="mt-0.5 text-lg font-medium text-white">
                   Bofulo Newton
                 </p>
                 <div className="flex justify-center gap-0.5 text-[#ff5001]">
@@ -131,21 +133,25 @@ function Review() {
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                   </svg>
                 </div>
-
-
               </div>
             </div>
           </blockquote>
         </div>
 
-        <div className="bg-[##ff5001] flex">
-          <div></div>
-          <div>
-            <h2>People who like to eat are always best food</h2>
-
-            <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
-
-            <button>Explore now</button>
+        <div className="bg-[#ff5001] flex flex-row items-end mt-[10rem] rounded-lg shadow-lg p-8 relative w-[100%] h-[50vh]">
+          <img
+            src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1400"
+            alt=""
+            className="absolute h-80 -top-10 -left-10 rounded-lg"
+          />
+          <div className="w-[50%] flex flex-col gap-10 text-white absolute right-0 top-10 items-start">
+            <h2 className="text-4xl w-[440px]">People who like to eat are always best food</h2>
+            <p className="w-[440px]">
+              In publishing and graphic design, Lorem ipsum is a placeholder
+              text commonly used to demonstrate the visual form of a document or
+              a typeface without relying on meaningful content.
+            </p>
+            <button className="border rounded-3xl border-white px-6 py-2 shadow-sm shadow-white">Explore now</button>
           </div>
         </div>
       </div>
