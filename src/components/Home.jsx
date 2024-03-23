e4bd34b8b39ca28c0ebfce9271c1b83cd84987b4
Newton Bofulo <newtonombese1@gmail.com>
2024-03-23 06:39:17 +0300

    Setting up responsiveness for the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className='bg-[url(https://images.pexels.com/photos/1482803/pexels-photo-1482803.jpeg?auto=compress&cs=tinysrgb&w=1600)] h-[100vh] bg-cover bg-center bg-no-repeat flex flex-col justify-center text-white px-[40px] sm:px-[140px]'>
-      <div className='w-3/4 flex flex-col gap-y-4'>
-        <h1 className='text-8xl'>Enjoy your <br/>delicious Food</h1>
-        <p className='text-[24px] w-[580px]'>Just order and wait for a while we&apos;ll be there at your door. Delivering the best foods for you. We are delivering the best foods.</p>
-        <div className='flex gap-4 mt-12'>
+    <div className='bg-[url(https://images.pexels.com/photos/1482803/pexels-photo-1482803.jpeg?auto=compress&cs=tinysrgb&w=1600)] h-[100vh] bg-cover bg-center bg-no-repeat flex flex-col justify-center text-white px-4 sm:px-10 md:px-20 lg:px-40'>
+      <div className='w-full flex flex-col gap-y-4'>
+        <h1 className='text-5xl sm:text-6xl md:text-7xl lg:text-8xl'>Enjoy your <br/>delicious Food</h1>
+        <p className='text-base sm:text-lg md:text-xl lg:text-2xl max-w-[580px]'>Just order and wait for a while we'll be there at your door. Delivering the best foods for you. We are delivering the best foods.</p>
+        <div className='flex flex-col sm:flex-row gap-4 mt-8 sm:mt-12'>
           <button className='bg-[#E14B00] text-white font-bold px-4 py-2 rounded-[6px] border-0 hover:bg-white hover:text-[#E14B00]'>Order Now</button>
-          <button className='border border-[#E14B00] rounded-[6px] text-[#E14B00] px-4 py-2 hover:border-white hover:text-white'>View Menu</button>
+          <button className='border border-[#E14B00] rounded-[6px] text-[#E14B00] px-4 py-2 hover:border-white hover:text-white mt-2 sm:mt-0'>View Menu</button>
         </div>
       </div>
-      <div></div>
     </div>
   );
 }
