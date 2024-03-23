f09f768d54903ccefcabb32fc0adb3737bae61a1
Newton Bofulo <newtonombese1@gmail.com>
2024-03-23 06:39:32 +0300

    Setting up responsiveness for the about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,24 +2,25 @@ import React from "react";
 
 function About() {
   return (
-    <div className="h-[100vh] flex gap-24 items-center justify-center">
+    <div className="h-auto sm:h-[100vh] flex flex-col sm:flex-row gap-4 sm:gap-24 items-center justify-center">
       <img
         src="https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg?auto=compress&cs=tinysrgb&w=1600"
-        className="w-1/2"
+        className="w-full sm:w-1/2"
+        alt="About Us"
       />
-      <div className="w-[420px] gap-8 flex flex-col items-start">
-        <h2 className="text-4xl">About</h2>
-        <p>
+      <div className="w-full sm:w-[420px] gap-4 sm:gap-8 flex flex-col items-center sm:items-start">
+        <h2 className="text-2xl sm:text-4xl">About</h2>
+        <p className="text-sm sm:text-base">
           In publishing and graphic design, Lorem ipsum is a placeholder text
           commonly used to demonstrate the visual form of a document or a
           typeface without relying on meaningful content. Lorem ipsum may be
-          used as placeholder before final copy is available.
+          used as a placeholder before the final copy is available.
         </p>
-        <p>
-          Our restaurant is open 24 hours and we are providing the best service
-          for our honorable clients.
+        <p className="text-sm sm:text-base">
+          Our restaurant is open 24 hours, and we are providing the best
+          service for our honorable clients.
         </p>
-        <button className="bg-[#E14B00] text-white font-bold px-6 py-3 rounded-[6px] border-0 hover:bg-white hover:text-[#E14B00]">
+        <button className="bg-[#E14B00] text-white font-bold px-4 py-2 rounded-[6px] border-0 hover:bg-white hover:text-[#E14B00]">
           Learn More
         </button>
       </div>
